3d16c501e76ef631d7a84d2b9f1999c02e4dd31e
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2021-04-13 16:16:34 +0530

    Added NoSQL DB

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+mongoose.connect('mongodb://localhost:27017/BlogPosts', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postSchema = new mongoose.Schema({
+	title : String,
+	postBody : String
+});
+const Blogs = new mongoose.model('Blogs',postSchema);
+
+const homeStartingContent = "Lorem Ipsum Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const blogs = [];
+const blogArr = [];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+Blogs.find({}, function(err,blogposts){
+	if(err)
+		console.log(err);
+	if(blogposts.length > 0 && blogposts.length !== null){
+		for(let i = 0; i < blogposts.length; i++)
+			blogArr.push(blogposts[i]);
+	}
+});
+
 app.get("/", function(req, res){
-	res.render("home", {homeContent : homeStartingContent, posts : blogs});
+	res.render("home", {homeContent : homeStartingContent, posts : blogArr});
 });
 
 app.get("/about", function(req, res){
@@ -34,24 +50,30 @@ app.get("/compose", function(req, res){
 
 app.get("/posts/:tweets", function(req, res){
 	const checkKey = _.lowerCase(req.params.tweets);
-	console.log(checkKey);
-	for(let i = 0; i < blogs.length; i++){
-		console.log(_.lowerCase(blogs[i].title));
-		if(_.lowerCase(blogs[i].title) === checkKey){
-			res.render("post", {blogTitle : blogs[i].title, content : blogs[i].postBody});
-		}//
+	for(let i = 0; i < blogArr.length; i++){
+		console.log(_.lowerCase(blogArr[i].title));
+		if(_.lowerCase(blogArr[i]._id) === checkKey){
+			res.render("post", {blogTitle : blogArr[i].title, content : blogArr[i].postBody});
+		}
 		else
-			console.log("Dorime!");
+			console.log("404! No such blog found");
 	}
 });
 
 app.post("/compose", function(req, res){
-	let post = {
+	let newBlog = new Blogs({
 		title : req.body.title,
 		postBody : req.body.tweet
-	};//
-	blogs.push(post);
-	res.redirect("/");
+	});
+	newBlog.save(function(err){
+		if(err)
+			console.log(err);
+		else{
+			blogArr.push(newBlog);
+			console.log("Your Blog has been successfully published!");
+			res.redirect("/");
+		}
+	});
 });
 
 
